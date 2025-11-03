51755290b7c4be5b10a3ebac64eddb1a606ac1d5
Amit <amitgupta561710@gmail.com>
2025-11-03 22:35:03 +0530

    ---

--- Rust/array.rs
+++ Rust/array.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let nums = [10, 20, 30, 40, 50];  // fixed size -> space in stack
     println!("{}", nums[0]);
@@ -6,5 +8,18 @@ fn main() {
     let tup = (10, 20, true, "Amit");
     println!("{}", tup.1);
     println!("{:?}", tup);
+
+
+    let mut fruits = vec!["apple", "banana"];
+    fruits.push("cherry");
+
+    println!("Last fruit: {}", fruits[2]);
+
+
+    let mut capitalCities = HashMap::new();
+    capitalCities.insert("France", "Paris");
+    capitalCities.insert("Japan", "Tokyo");
+
+    println!("Capital of Japan is {}", capitalCities["Japan"]);
 
 }
--- Rust/condition.rs
+++ Rust/condition.rs
@@ -7,4 +7,29 @@ fn main(){
     else{
         println!("Drive")
     }
+
+    let day = 4;
+    match day {
+        1 => println!("Monday"),
+        2 => println!("Tuesday"),
+        3 => println!("Wednesday"),
+        4 => println!("Thursday"),
+        5 => println!("Friday"),
+        6 => println!("Saturday"),
+        7 => println!("Sunday"),
+        _ => println!("Invalid day."),
+    }
+
+
+    let result = match day {
+        1 | 2 | 3 | 4 | 5 => "Weekday",
+        6 | 7 => "Weekend",
+        _ => "Invalid day",
+    }
+
+    let mut count = 1;
+    while count <= 5 {
+        println!("Count: {}", count);
+        count += 1;
+    }
 }
